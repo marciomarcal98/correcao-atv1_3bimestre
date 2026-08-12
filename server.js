e8cbfcf6b0c2887fbf1fa80b6052099d44108f18
Marcio Marçal da Costa Garcia <48536961+marciomarcal98@users.noreply.github.com>
2026-08-12 07:41:41 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,81 @@
+import express from "express"
+import mysql2 from "mysql2"
+
+const PORT = 3000
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (request, response) => {
+    const selectCommand = "SELECT * FROM correcao_MarcioMarcal"
+
+    database.query(selectCommand, (error, data) => {
+        if (error) {
+            console.log(error)
+            return
+        }
+
+        response.json(data)
+    })
+})
+
+app.post("/create-movie", (request, response) => {
+    const { title, gender, ageLimit, duration } = request.body
+
+    const insertCommand = "INSERT INTO correcao_MarcioMarcal(title, gender, ageLimit, duration) VALUES (?, ?, ?, ?)"
+
+    database.query(insertCommand, [title, gender, ageLimit, duration], (error) => {
+        if(error) {
+            console.log(error)
+        } else {
+            response.status(201).json({
+                message: "Filme criado com sucesso!"
+            })
+        }
+    })
+})
+
+app.delete("/delete-movie/:id", (request, response) => {
+    const { id } = request.params
+
+    const deleteCommand = "DELETE FROM correcao_MarcioMarcal WHERE id=?"
+
+    database.query(deleteCommand, [id], (error) => {
+        if (error) {
+            console.log(error)
+        } else {
+            response.json({
+                message: "Filme removido com sucesso!"
+            })
+        }
+    })
+})
+
+app.put("/edit-movie/:id", (request, response) => {
+    const { id } = request.params
+    const { title, gender, ageLimit, duration } = request.body
+
+    const updateCommand = "UPDATE correcao_MarcioMarcal SET title = ?, gender = ?, ageLimit = ?, duration = ? WHERE id = ?"
+
+    database.query(updateCommand, [title, gender, ageLimit, duration, id], (error) => {
+        if (error) {
+            console.log(error)
+            return
+        }
+        
+        response.json({
+	        message: "Filme editado com sucesso!"
+        })
+    })})
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
+
+const database = mysql2.createPool({
+    database: "alunos_filmes_03MA",
+    host: "benserverplex.ddns.net",
+    user: "alunos",
+    password: "senhaAlunos",
+    connectionLimit: 10
+})
