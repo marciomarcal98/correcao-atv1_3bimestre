3ce766446288194ab898d8814dc03f0930d874de
Marcio Marçal da Costa Garcia <48536961+marciomarcal98@users.noreply.github.com>
2026-08-19 07:23:16 -0300

    frontend da rota GET

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express"
 import mysql2 from "mysql2"
+import cors from "cors"
 
 const PORT = 3000
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/", (request, response) => {
     const selectCommand = "SELECT * FROM correcao_MarcioMarcal"
